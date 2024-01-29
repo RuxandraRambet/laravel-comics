534d488f79e8d8d153462938cc38d7dce573105e
RuxandraRambet <ruxandraramb@gmail.com>
2024-01-29 18:13:54 +0100

    feat: header menu and logo

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,61 @@
+<?php 
+$menu = [
+    [
+        'name' => 'characters',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'comics',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'movies',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'tv',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'games',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'collectibles',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'videos',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'fans',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'news',
+        'link' => '#', 
+    ],
+    [
+        'name' => 'shop',
+        'link' => '#', 
+    ],
+]
+?>
+<header>
+    <div class="container d-flex align-items-center justify-content-between p-5">
+        <div class="logo-container">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+        </div>
+        <nav>
+            <ul class="list-unstyled d-flex align-items-center text-uppercase gap-3">
+                @foreach ($menu as $item )
+                    <li>
+                        <a href="{{$item['link']}}">{{$item['name']}}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </nav>
+
+    </div>
+</header>
