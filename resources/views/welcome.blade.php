b7b42ab08cbb706938794bcd86da44205255f448
RuxandraRambet <ruxandraramb@gmail.com>
2024-01-29 19:04:38 +0100

    feat: comics cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,22 @@
 @extends ('layouts.main')
 
 @section('main')
-<div class="jumbo"></div>
+    <div class="jumbo"></div>
+
+    <div class="cards-section">
+        <div class="container">
+            <h3 class="text-uppercase text-white">current series</h3>
+            <ul class="comic-cards d-flex flex-wrap">
+                @foreach ($comics as $comic)
+                    <li class="comic-card">
+                        <a href="#"><img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}"></a>
+                        <h5 class="text-uppercase text-white">{{ $comic['series'] }}</h5>
+                    </li>
+                @endforeach
+            </ul>
+            <div class="button-container">
+                <button class="text-uppercase">load more</button>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'comics' => config('comicsdb')
+    ];
+
+    return view('welcome', $data);
 });
